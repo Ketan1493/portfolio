b2da8ea91856566287e9f2dc5338eb15d1234428
Yash Kadam <yash11398@gmail.com>
2022-06-20 17:53:00 -0400

    update mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
 $(window).load(function() {
-    if ($(window).width() > 1000) {
         $(".navbar").css("display", "none");
+        $(".main").css("display", "none");
         $(".navlinks").css("display", "none");
+        $(".info-card").css("left", "-100%");
         if ($(window).width() > 1000) {
             $(".navbar").delay(100).slideDown(300);
+            $(".main").delay(300).fadeIn(300, function(){
+                $(".info-card").animate({
+                    left: "30%"
+                }, 500, function(){
+                });
+                $(".footer").delay(100).fadeIn(300);
+            });
             $(".navlinks").delay(100).fadeIn(300);
-            $(".footer").delay(100).fadeIn(300);
         }
-    }
+        else{
+            $(".navbar").delay(100).slideDown(300);
+            $(".main").delay(300).fadeIn(300, function(){
+                $(".footer").delay(100).fadeIn(300);
+            });
+        }
 });
